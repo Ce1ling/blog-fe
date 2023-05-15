c57716b26890d8819521d105cece713d7f958e2b
Ce1ling <2134045201@qq.com>
2023-05-15 17:59:02 +0800

    fix: fix blog date bug

--- src/pages/Blog/BlogDetails/BlogDetails.tsx
+++ src/pages/Blog/BlogDetails/BlogDetails.tsx
@@ -41,7 +41,7 @@ const BlogDetails: React.FC = () => {
     setBlogDetails(res.data[0])
   }
   const getDate = (timestamp?: number) => {
-    const date = timestamp ? new Date(timestamp) : new Date()
+    const date = timestamp ? new Date(timestamp * 1000) : new Date()
     const year = date.getFullYear()
     const month = (date.getMonth() + 1).toString().padStart(2, '0')
     const day = date.getDate().toString().padStart(2, '0')
