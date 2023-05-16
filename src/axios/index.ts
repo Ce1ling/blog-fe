aba0163982b8ade505bdaaa852a0158d00aa759e
Ce1ling <2134045201@qq.com>
2023-05-16 11:05:19 +0800

    feat: handle api request error

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { message } from 'antd'
+
+import type { AxiosError } from 'axios'
+
 
 const server = axios.create({
   baseURL: 'http://localhost:3211',
@@ -8,10 +12,17 @@ const server = axios.create({
 server.interceptors.request.use(config => {
   config.headers['Content-Type'] = 'application/json;charset=utf-8'
   return config
+}, (err: AxiosError) => {
+  return Promise.reject(err)
 })
 
 server.interceptors.response.use(response => {
   return response
+}, (err: AxiosError) => {
+  if (err.code === 'ERR_NETWORK') {
+    message.error('网络错误, 获取数据失败')
+  }
+  return Promise.reject(err)
 })
 
 export default server
